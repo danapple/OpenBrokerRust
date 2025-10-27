b96adf139a10633498e12eb086c2476090c9bc11
Daniel I. Applebaum <danapple@danapple.com>
2025-10-27 14:51:45 +0000

    Make failure in getting system time explicit.

--- src/time.rs
+++ src/time.rs
@@ -1,5 +1,9 @@
 use std::time::SystemTime;
 
 pub fn current_time_millis() -> i64 {
-    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).ok().unwrap().as_millis() as i64
+    let system_time_result = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
+    match system_time_result {
+        Ok(system_time) => system_time.as_millis() as i64,
+        Err(time_error) => panic!("Unable to get system time: {}", time_error.to_string()),
+    }
 }
