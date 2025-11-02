6c93b71a82ca1f19fc4287110427c828b967b2b9
Daniel I. Applebaum <danapple@danapple.com>
2025-11-02 18:50:14 +0000

    Enhance web application to allow placing orders.

--- resources/static/app/js/app.js
+++ resources/static/app/js/app.js
@@ -66,13 +66,20 @@ function connect() {
     getInstruments();
 }
 
+function getInstrumentSymbol(instrument_key) {
+    var instrument = instruments[instrument_key];
+    if (!instrument) {
+        return "Id: " + instrument_key;
+    }
+    return instrument.symbol;
+}
+
 function getInstrumentDescription(instrument_key) {
     var instrument = instruments[instrument_key];
     if (!instrument) {
         return "Id: " + instrument_key;
     }
-  //  console.log("Instrument description for display" + instrument.description);
-    return instrument.symbol + "(" + instrument.description + ")";
+    return instrument.description;
 }
 
 function handleDepth(stompMessage) {
@@ -80,9 +87,11 @@ function handleDepth(stompMessage) {
    // console.log('Got depth message: ' + message);
     let depth = JSON.parse(message);
     let description = getInstrumentDescription(depth.instrument_key);
+    let symbol = getInstrumentSymbol(depth.instrument_key);
+
     $("#markets_body").append(
         "<tr>"
-        + "<td>" + description + "</td>"
+        + "<td title='" + description + "'>" + symbol + "</td>"
         + "<td>" + message + "</td>"
         + "</tr>");
 }
@@ -93,50 +102,80 @@ function handleLastTrade(stompMessage) {
   //  console.log('Got last trade message: ' + message);
     let last_trade = JSON.parse(message);
     let description = getInstrumentDescription(last_trade.instrument_key);
+    let symbol = getInstrumentSymbol(last_trade.instrument_key);
 
     $("#markets_body").append(
         "<tr>"
-        + "<td>"+ description + "</td>"
+        + "<td title='" + description + "'>" + symbol + "</td>"
         + "<td>" + message + "</td>"
         + "</tr>");
 }
 
 function handleBalance(balance) {
     if (balance !== undefined && balance !== null) {
-        $("#balances_body").append(
-            "<tr>"
-            + "<td>" + balance.cash + "</td>"
+        deleteRow('posbalance');
+        $("#positions_body").append(
+            "<tr id=posbalance>"
+            + "<td title='cash balance'>-cash-</td>"
+            + "<td></td>"
+            + "<td></td>"
+            + "<td>" + (balance.cash).toFixed(2) + "</td>"
+            + "<td></td>"
+            + "<td></td>"
             + "</tr>");
+    }
+}
+
+function deleteRow(rowid)
+{
+    var row = document.getElementById(rowid);
+    if (row !== null) {
+        row.parentNode.removeChild(row);
     }
 }
 
 function handlePosition(position) {
     if (position !== undefined && position !== null) {
+        let id = "pos:" + position.instrument_key;
+        deleteRow(id);
+
+        let description = getInstrumentDescription(position.instrument_key);
+        let symbol = getInstrumentSymbol(position.instrument_key);
+
         $("#positions_body").append(
-            "<tr>"
-            + "<td>" + getInstrumentDescription(position.instrument_key) + "</td>"
+            "<tr id=" + id + ">"
+            + "<td title='" + description + "'>" + symbol + "</td>"
             + "<td>" + position.quantity + "</td>"
             + "<td>" + position.cost + "</td>"
+            + "<td>" + (position.cost * position.quantity).toFixed(2) + "</td>"
             + "<td>" + position.closed_gain + "</td>"
             + "<td>  <button id=\"close\" class=\"btn btn-default\" type=\"submit\">Close</button>\n </td>"
-            + "</tr>");
+            + "</tr id=" + position.instrument_key + ">");
     }
 }
 
 function handleOrderState(orderState) {
     if (orderState !== undefined && orderState !== null) {
+        let id = "ord:" + orderState.order.ext_order_id;
+        deleteRow(id);
+
+        let symbol = "";
         let description = "";
         for (const [_, leg] of Object.entries(orderState.order.legs)) {
             if (description.length > 0) {
                 description += "/";
             }
             description += getInstrumentDescription(leg.instrument_key);
+            if (symbol.length > 0) {
+                symbol += "/";
+            }
+            symbol += getInstrumentSymbol(leg.instrument_key);
         }
 
         $("#orders_body").append(
-            "<tr>"
+            "<tr id=" + id + ">"
             + "<td>"+ orderState.order.order_number + "</td>"
-            + "<td>" + description + "</td>"
+            + "<td title='" + description + "'>" + symbol + "</td>"
             + "<td>" + orderState.order_status + "</td>"
             + "<td>"+ orderState.order.quantity + "</td>"
             + "<td>"+ orderState.order.price + "</td>"
@@ -159,14 +198,19 @@ function handleUpdate(stompMessage) {
 function processAccounts(account_data) {
    // console.log('Got accounts:' + JSON.stringify(account_data));
     $("#account-data").show();
+    let order_account_select = document.getElementById('order_account');
 
     for (account of account_data) {
     //    console.log('account: ' + JSON.stringify(account));
         $("#accounts_body").append(
             "<tr>"
             + "<td>"+ account.account_number + "</td>"
-            + "<td>" + account.privileges + "</td>");
+            + "<td>" + account.account_name + "</td>");
         subscribeAccount(account.account_key, handleUpdate);
+        var opt = document.createElement('option');
+        opt.value = account.account_key;
+        opt.innerHTML = account.account_number;
+        order_account_select.appendChild(opt);
         break;
     }
 }
@@ -174,18 +218,25 @@ function processAccounts(account_data) {
 function processInstruments(instrument_data) {
  //   console.log('Got instruments:' + JSON.stringify(instrument_data));
     instruments = instrument_data;
+    let order_instrument_select = document.getElementById('order_instrument');
 
     Object.values(instruments).forEach((instrument) => {
   //      console.log('instrument: ' + JSON.stringify(instrument));
         subscribe('/markets/' + instrument.instrument_key + '/depth', handleDepth);
         subscribe('/markets/' + instrument.instrument_key + '/last_trade', handleLastTrade);
+        var opt = document.createElement('option');
+        opt.value = instrument.instrument_key;
+        opt.innerHTML = instrument.description;
+        order_instrument_select.appendChild(opt);
     })
+
     getAccounts();
+
     stompClient.activate();
 }
 
 function getAccounts() {
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             processAccounts(JSON.parse(this.responseText));
@@ -197,7 +248,7 @@ function getAccounts() {
 }
 
 function getInstruments() {
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             processInstruments(JSON.parse(this.responseText));
@@ -207,5 +258,42 @@ function getInstruments() {
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
+
+function submitOrder() {
+    let quantity = document.getElementById("order_quantity").value;
+    let price = document.getElementById("order_price").value;
+    let instrument_key = document.getElementById("order_instrument").value;
+    let account_key = document.getElementById("order_account").value;
+
+    // alert("Place order for " + quantity + " @ " + price + " for " + instrument_key);
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //processInstruments(JSON.parse(this.responseText));
+        }
+    };
+    let path = "/accounts/" + account_key + "/orders";
+    xhttp.open("POST", path, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    body = JSON.stringify({
+        price: Number(price),
+        quantity: Number(quantity),
+        legs: [
+            {
+                ratio: Numbergit statu(1),
+                instrument_key: instrument_key
+            }
+        ]
+    });
+    xhttp.send(body);
+
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#order_submit" ).click(() => submitOrder());
+});
 
 connect();
